a82bb4ed7ad73f02b2598b415dc31b1ff6b4e676
MAKYOON <k.li@ludus-academie.com>
2019-12-27 15:45:23 +0100

    27/12/19
    Commit du cours numéro 3, présence d'exécutable dans le dossier
    Voir le READ_ME pour plus d'informations

---
+++ Unit3/Prototype_3/Assets/Course Library/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private PlayerController playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        audioSource = GetComponent<AudioSource>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController.gameOver == true)
+        {
+            audioSource.Stop();
+        }
+    }
+}
---
+++ Unit3/Prototype_3/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtSplatter;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource audioSource;
+
+    public float jumpForce = 10;
+    public float gravityModifier = 2;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtSplatter.Stop();
+            audioSource.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Challenge 3");
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtSplatter.Play();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtSplatter.Stop();
+            audioSource.PlayOneShot(crashSound, 1.0f);
+
+        }
+    }
+}
